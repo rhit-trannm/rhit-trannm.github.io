e67f53d5a563226d5f93aacea3ba8d768cd3f8e6
Nhan Tran <slippinice@gmail.com>
2025-03-25 14:18:02 -0500

    Tailwind & PrimeVue

--- project/env.d.ts
+++ project/env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
+
+// PrimeVue module declarations
+declare module 'primevue/config'
+declare module 'primevue/button'
+declare module 'primevue/*'
